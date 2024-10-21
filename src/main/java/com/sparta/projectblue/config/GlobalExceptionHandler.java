b4b0165e26074550dbadfeab7a2897b63b5e9540
everydayspring <lee.bom.952@gmail.com>
2024-10-21 20:35:12 +0900

    ExceptionHandler

---
+++ src/main/java/com/sparta/projectblue/config/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.sparta.projectblue.config;
+
+import jakarta.security.auth.message.AuthException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<?>> invalidRequestExceptionException(
+            IllegalArgumentException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getErrorResponse(status, ex.getMessage());
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ApiResponse<?>> handleAuthException(AuthException ex) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return getErrorResponse(status, ex.getMessage());
+    }
+
+    public ResponseEntity<ApiResponse<?>> getErrorResponse(HttpStatus status, String message) {
+
+        return ResponseEntity.status(status).body(ApiResponse.error("오류가 발생했습니다." + message));
+    }
+}
---
+++ src/main/java/com/sparta/projectblue/domain/common/exception/ServerException.java
@@ -0,0 +1,9 @@
+package com.sparta.projectblue.domain.common.exception;
+
+public class ServerException extends RuntimeException {
+
+    public ServerException(String message) {
+        super(message);
+    }
+}
+
