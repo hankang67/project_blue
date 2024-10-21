ffcd6a9346845000bae41d7fc953b4d03fdda656
everydayspring <lee.bom.952@gmail.com>
2024-10-21 20:24:24 +0900

    init

---
+++ src/main/java/com/sparta/projectblue/ProjectBlueApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.projectblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBlueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/projectblue/ProjectBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.projectblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
