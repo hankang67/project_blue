bc7d89c0380db54e52e3c1523d171eb9ce3cf314
everydayspring <lee.bom.952@gmail.com>
2024-10-22 14:29:12 +0900

    swagger 적용

--- src/main/java/com/sparta/projectblue/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/sparta/projectblue/domain/reservation/controller/ReservationController.java
@@ -4,6 +4,8 @@ import com.sparta.projectblue.config.ApiResponse;
 import com.sparta.projectblue.domain.common.dto.AuthUser;
 import com.sparta.projectblue.domain.reservation.dto.CreateReservationDto;
 import com.sparta.projectblue.domain.reservation.service.ReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
+@Tag(name = "Reservation", description = "공연 예매 API")
 public class ReservationController {
 
     private final ReservationService reservationService;
 
     @PostMapping
+    @Operation(summary = "예매", description = "공연 예매에 사용하는 API")
     public ResponseEntity<ApiResponse<?>> create(
             @AuthenticationPrincipal AuthUser authUser,
             @Valid @RequestBody CreateReservationDto.Request request) {
