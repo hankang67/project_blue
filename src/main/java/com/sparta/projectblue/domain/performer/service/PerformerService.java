8a052b853474aa5376506d6a74c6ed335cede1d0
kang <m0nsia67@gmail.com>
2024-10-23 18:25:13 +0900

    update_Performer 등록 기능 생일 예외처리

--- src/main/java/com/sparta/projectblue/domain/performer/service/PerformerService.java
+++ src/main/java/com/sparta/projectblue/domain/performer/service/PerformerService.java
@@ -4,9 +4,13 @@ import com.sparta.projectblue.domain.performance.repository.PerformanceRepositor
 import com.sparta.projectblue.domain.performer.dto.PerformerDetailDto;
 import com.sparta.projectblue.domain.performer.entity.Performer;
 import com.sparta.projectblue.domain.performer.repository.PerformerRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +22,17 @@ public class PerformerService {
 
     //출연자 생성
     @Transactional
-    public PerformerDetailDto.Response createPerformer(PerformerDetailDto requestDto) {
+    public PerformerDetailDto.Response createPerformer(@Valid PerformerDetailDto requestDto) {
+        try {
+            LocalDate birthDate = LocalDate.parse(requestDto.getBirth());
+            if (birthDate.isBefore(LocalDate.of(1900, 1, 1)) || birthDate.isAfter(LocalDate.now())) {
+                throw new IllegalArgumentException("생일은 현재 날짜보다 이전이거나 1900년도 이후의 날짜여야 합니다.");
+            }
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("생일은 yyyy-MM-dd 형식이어야 합니다.");
+        }
+
+
         Performer performer = new Performer(requestDto.getName(), requestDto.getBirth(), requestDto.getNation());
         Performer savedPerformer = performerRepository.save(performer);
         return new PerformerDetailDto.Response(savedPerformer.getName(), savedPerformer.getBirth(), savedPerformer.getNation());
