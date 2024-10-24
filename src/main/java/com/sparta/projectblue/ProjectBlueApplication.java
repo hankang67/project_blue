07fd00109187301d2b6f18bc82aa0d5dcac5253c
kang <dmschd873@gmail.com>
2024-10-24 21:37:13 +0900

    [fix] 예매취소시 slack 알림기능 정상 작동 확인

--- src/main/java/com/sparta/projectblue/ProjectBlueApplication.java
+++ src/main/java/com/sparta/projectblue/ProjectBlueApplication.java
@@ -3,8 +3,7 @@ package com.sparta.projectblue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
