079129a543888e7d8a8803312e0f8dfe0904c59c
kang <dmschd873@gmail.com>
2024-11-07 18:03:31 +0900

    [add] Coupon 선착순 발행 로직 수정 및 알림 전송 정보 수정, 쿠폰테스트코드 수정

--- src/main/java/com/sparta/projectblue/config/DistributedLock.java
+++ src/main/java/com/sparta/projectblue/config/DistributedLock.java
@@ -17,8 +17,8 @@ public @interface DistributedLock {
     TimeUnit timeUnit() default TimeUnit.SECONDS;
 
     // 락 획득을 위해 waitTime 만큼 대기
-    long waitTime() default 3L;
+    long waitTime() default 4L;
 
     // 락을 획득한 이후 leaseTime 이 지나면 락을 해제
-    long leaseTime() default 7L;
+    long leaseTime() default 8L;
 }
