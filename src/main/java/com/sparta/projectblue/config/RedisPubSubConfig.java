cdfaa0784297f33ae574d587f2b83e34c9c058f2
kang <dmschd873@gmail.com>
2024-11-13 11:49:21 +0900

    SCRUM-156 [update] Redis pub/sub config

---
+++ src/main/java/com/sparta/projectblue/config/RedisPubSubConfig.java
@@ -0,0 +1,34 @@
+package com.sparta.projectblue.config;
+
+import com.sparta.projectblue.sse.RedisSubscriber;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisPubSubConfig {
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory redisConnectionFactory,
+            MessageListenerAdapter messageListenerAdapter,
+            ChannelTopic channelTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, channelTopic);
+        return container;
+    }
+
+    @Bean  // 메세지 처리 메서드
+    public MessageListenerAdapter messageListenerAdapter(RedisSubscriber redisSubscriber) {
+        return new MessageListenerAdapter(redisSubscriber, "handleNotificationMessage");
+    }
+
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("notification-channel");
+    }
+}
