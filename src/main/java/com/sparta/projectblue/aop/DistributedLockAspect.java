f5b101462f09a47dddd6089da37a21f5c52fa326
kang <dmschd873@gmail.com>
2024-11-15 18:06:43 +0900

    SCRUM-166 [fix] DistributedLockAspect 예외처리 수정 및 제거

--- src/main/java/com/sparta/projectblue/aop/DistributedLockAspect.java
+++ src/main/java/com/sparta/projectblue/aop/DistributedLockAspect.java
@@ -49,20 +49,17 @@ public class DistributedLockAspect {
                             distributedLock.leaseTime(),
                             distributedLock.timeUnit());
             if (!available) {
-                return new IllegalStateException("락을 획득하지 못했습니다.");
+                throw new IllegalStateException("락을 획득하지 못했습니다.");
             }else{
                 log.info("락을 획득");
             }
             return aopForTransaction.proceed(joinPoint); // 락 획득 시 메서드 호출하여 실제 작업 수행
         } catch (InterruptedException e) {
-            throw new InterruptedException("인터럽트 발생");
+            log.warn("인터럽트 발생", e);
+            throw e;
         } finally {
-            try {
                 lock.unlock();
                 log.info("lock 해제 완료");
-            } catch (IllegalMonitorStateException e) {
-                log.info("lock 이 이미 종료 되었습니다.");
-            }
         }
     }
 }
