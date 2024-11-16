7333cc7c4b4c817cfbbd6f222866de50f889fc1f
summer <gupy1998@gmail.com>
2024-11-16 21:48:21 +0900

    SCRUM-186 Remove usage of generic wildcard type.

--- src/main/java/com/sparta/projectblue/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/projectblue/domain/review/controller/ReviewController.java
@@ -1,20 +1,18 @@
 package com.sparta.projectblue.domain.review.controller;
 
-import com.sparta.projectblue.domain.review.dto.CreateReviewResponseDto;
-import com.sparta.projectblue.domain.review.dto.UpdateReviewResponseDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
 import com.sparta.projectblue.config.ApiResponse;
 import com.sparta.projectblue.domain.common.dto.AuthUser;
 import com.sparta.projectblue.domain.review.dto.CreateReviewRequestDto;
+import com.sparta.projectblue.domain.review.dto.CreateReviewResponseDto;
 import com.sparta.projectblue.domain.review.dto.UpdateReviewRequestDto;
+import com.sparta.projectblue.domain.review.dto.UpdateReviewResponseDto;
 import com.sparta.projectblue.domain.review.service.ReviewService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reviews")
